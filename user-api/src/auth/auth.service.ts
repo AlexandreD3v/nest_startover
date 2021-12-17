8464a9e3d3e882d3a1c230377c374bc5db23a528
Alexandre J. Corrêa <alexandre.j.correa@hotmail.com>
2021-12-17 16:36:31 -0300

    Autentificacao

--- user-api/src/auth/auth.service.ts
+++ user-api/src/auth/auth.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { UserRepository } from '../users/users.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {}
+
+}
